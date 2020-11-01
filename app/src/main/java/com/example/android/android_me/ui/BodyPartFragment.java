3cd614a0fc0efb5b8ae658b114d65a7d236f08a0
liuchenyang0515 <897418643@qq.com>
2020-11-01 18:18:05 +0800

    从fragment生命周期到创建、替换、点击事件、旋转状态保存放在step01分支

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,140 @@
+package com.example.android.android_me.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BodyPartFragment extends Fragment {
+
+    private static final String TAG = "BodyPartFragment";
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "list_index";
+    private List<Integer> mImageIds;
+
+    private int mListIndex;
+
+    public BodyPartFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView: ");
+        if (savedInstanceState != null) { // 还要在activity注意处理一下
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
+        }
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        final ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        if (mImageIds != null) {
+            imageView.setImageResource(mImageIds.get(mListIndex));
+            final int size = mImageIds.size();
+            // fragment要复用，监听事件不要写在activity，否则每次new一个fragment都要绑定监听
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mListIndex < size - 1) {
+                        ++mListIndex;
+                    } else {
+                        mListIndex = 0;
+                    }
+                    imageView.setImageResource(mImageIds.get(mListIndex));
+                }
+            });
+        } else {
+            Log.v(TAG, "This fragment has a null list of image id's");
+        }
+        return rootView;
+    }
+
+    public void setImageIds(List<Integer> imageIds) {
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex(int index) {
+        mListIndex = index;
+    }
+     // 保存旋转钱的状态，避免旋转又还原从0开始
+
+    @Override
+    public void onSaveInstanceState(Bundle currentState) {
+        Log.i(TAG, "onSaveInstanceState: ");
+        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>)mImageIds);
+        currentState.putInt(LIST_INDEX, mListIndex);
+    }
+    // 以下方法探索生命周期使用，看看旋转方向会怎么样
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach: ");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: ");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.i(TAG, "onActivityCreated: ");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG, "onDestroyView: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG, "onDetach: ");
+    }
+}
